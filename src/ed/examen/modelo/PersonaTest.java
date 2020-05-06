4fdcffa1dbaad42bc9c52b151cdcd75f2ee16836
Luis <daw005.2020@gmail.com>
2020-05-06 09:36:46 +0200

    implementacion test<testPersona>

---
+++ src/ed/examen/modelo/PersonaTest.java
@@ -0,0 +1,44 @@
+package ed.examen.modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PersonaTest {
+
+	@Test
+	void testPersonaStringStringString() {
+		assertEquals("12345678A", "Luis", "Doblas");
+	}
+
+	@Test
+	void testGetDni() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetDni() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetApellido1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetApellido1() {
+		fail("Not yet implemented");
+	}
+
+}
